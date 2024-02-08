f8bb35df847b6becce0ee7fd86475537012b0838
Whitegolyb <kirillulianow@gmail.com>
2024-02-08 15:34:34 +0200

    added aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            src: '/src',
+            components: '/src/components',
+            assets: '/src/assets',
+            constants: '/src/constants',
+            utils: '/src/utils',
+            styles: '/src/styles',
+        },
+    },
+});
