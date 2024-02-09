6c044356909171669c27ad91c55230ed13563895
Whitegolyb <kirillulianow@gmail.com>
2024-02-09 16:36:01 +0200

    created Hero-section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,5 +1,75 @@
-const Computers = () => {
-    return <div>Computers</div>;
+/* eslint-disable react/no-unknown-property */
+import { Suspense, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import { CanvasLoader } from '../waiters';
+
+const Computers = ({ isMobile }) => {
+    const computer = useGLTF('./desktop_pc/scene.gltf');
+
+    return (
+        <mesh>
+            <hemisphereLight intensity={1.5} groundColor="black" />
+            <spotLight
+                position={[-20, 50, 10]}
+                angle={0.12}
+                penumbra={1}
+                intensity={1}
+                castShadow
+                shadow-mapSize={1024}
+            />
+            <pointLight intensity={1} />
+            <primitive
+                object={computer.scene}
+                scale={isMobile ? 0.7 : 0.75}
+                position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+                rotation={[-0.01, -0.2, -0.1]}
+            />
+        </mesh>
+    );
 };
 
-export default Computers;
+const ComputersCanvas = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const mediaQuerry = window.matchMedia('(max-width: 500px)');
+
+        setIsMobile(mediaQuerry.matches);
+
+        const handleMediaQuerryChange = e => {
+            setIsMobile(e.matches);
+        };
+
+        mediaQuerry.addEventListener('change', handleMediaQuerryChange);
+
+        return () => {
+            mediaQuerry.removeEventListener('change', handleMediaQuerryChange);
+        };
+    }, []);
+
+    return (
+        <Canvas
+            frameloop="demand"
+            shadows
+            camera={{ position: [20, 3, 5], fov: 25 }}
+            gl={{ preserveDrawingBuffer: true }}
+        >
+            <Suspense fallback={<CanvasLoader />}>
+                <OrbitControls
+                    enableZoom={false}
+                    maxPolarAngle={Math.PI / 2}
+                    minPolarAngle={Math.PI / 2}
+                />
+                <Computers isMobile={isMobile} />
+            </Suspense>
+            <Preload all />
+        </Canvas>
+    );
+};
+
+Computers.propTypes = { isMobile: PropTypes.bool };
+
+export default ComputersCanvas;
