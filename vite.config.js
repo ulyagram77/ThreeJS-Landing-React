e77655ad2e946025f5cb8c7a61a69401b2e963ae
Whitegolyb <kirillulianow@gmail.com>
2024-02-12 22:20:45 +0200

    created hook for getting dynamic screen width

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        legacy({
+            targets: ['defaults', 'not IE 11'],
+        }),
+    ],
     resolve: {
         alias: {
             src: '/src',
@@ -12,6 +18,7 @@ export default defineConfig({
             assets: '/src/assets',
             constants: '/src/constants',
             utils: '/src/utils',
+            hooks: '/src/hooks',
             styles: '/src/styles',
         },
     },
