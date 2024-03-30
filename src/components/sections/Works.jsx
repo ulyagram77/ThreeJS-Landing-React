82957a86855e2d33ccf2296b7408d4a47c2c8ddb
Whitegolyb <kirillulianow@gmail.com>
2024-03-30 22:25:47 +0200

    fix: trying to fix samsung issue #11

--- src/components/sections/Works.jsx
+++ src/components/sections/Works.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import { motion } from 'framer-motion';
 
-import { Tilt } from 'react-tilt';
 import { styles } from 'src/styles/styles';
 import { github } from 'src/assets';
 import { withSectionWrapper } from 'src/hoc';
@@ -18,14 +17,7 @@ const ProjectCard = ({
 }) => {
     return (
         <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
-            <Tilt
-                options={{
-                    max: 45,
-                    scale: 1,
-                    speed: 450,
-                }}
-                className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-            >
+            <div className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
                 <div className="relative w-full h-[230px]">
                     <img
                         src={image}
@@ -62,7 +54,7 @@ const ProjectCard = ({
                         </p>
                     ))}
                 </div>
-            </Tilt>
+            </div>
         </motion.div>
     );
 };
