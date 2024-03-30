a4a86e8cf009d734d33e531f5e599a74fbb3dd7a
Whitegolyb <kirillulianow@gmail.com>
2024-03-30 22:20:11 +0200

    fix: trying to fix samsung issue #11

--- src/components/sections/Works.jsx
+++ src/components/sections/Works.jsx
@@ -71,7 +71,7 @@ const Works = withSectionWrapper(() => {
     return (
         <>
             <motion.div variants={textVariant()}>
-                <p className={`${styles.sectionSubText} `}>showcase of my skills</p>
+                <p className={`${styles.sectionSubText} `}>My work</p>
                 <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
             </motion.div>
 
@@ -80,18 +80,11 @@ const Works = withSectionWrapper(() => {
                     variants={fadeIn('', '', 0.1, 1)}
                     className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
                 >
-                    Below you can watch some of my works. Following projects
-                    showcases my skills and experience through real examples of my
-                    work.
-                    <br />
-                    <br />
-                    Each project contains <b>demo link</b>, <b>source-code link</b>{' '}
-                    and <b>brief description</b> with the technical stack used in it.
-                    <br />
-                    <br />
-                    These projects reflect my ability to work with different
-                    situations and technologies, as well as my ability to organize
-                    the project beautifully for others to see.
+                    Following projects showcases my skills and experience through
+                    real-world examples of my work. Each project is briefly described
+                    with links to code repositories and live demos in it. It reflects
+                    my ability to solve complex problems, work with different
+                    technologies, and manage projects effectively.
                 </motion.p>
             </div>
 
