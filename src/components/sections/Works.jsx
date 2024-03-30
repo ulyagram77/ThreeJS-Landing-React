01e4136347cfe9622a62ca93886a603bcf1a2b22
Whitegolyb <kirillulianow@gmail.com>
2024-03-30 21:54:41 +0200

    fix: trying to fix samsung issue #10

--- src/components/sections/Works.jsx
+++ src/components/sections/Works.jsx
@@ -1,45 +1,46 @@
 /* eslint-disable react/prop-types */
 import { motion } from 'framer-motion';
 
+import { Tilt } from 'react-tilt';
 import { styles } from 'src/styles/styles';
-import { github, demo } from 'src/assets';
+import { github } from 'src/assets';
 import { withSectionWrapper } from 'src/hoc';
 import { projects } from 'src/constants';
 import { fadeIn, textVariant } from 'src/utils/motion';
 
-const ProjectCard = props => {
-    const { index, name, description, tags, image, source_code_link, demo_link } =
-        props;
-
+const ProjectCard = ({
+    index,
+    name,
+    description,
+    tags,
+    image,
+    source_code_link,
+}) => {
     return (
-        <motion.div variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}>
-            <div className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+        <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
+            <Tilt
+                options={{
+                    max: 45,
+                    scale: 1,
+                    speed: 450,
+                }}
+                className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+            >
                 <div className="relative w-full h-[230px]">
                     <img
                         src={image}
-                        alt={name}
+                        alt="project_image"
                         className="w-full h-full object-cover rounded-2xl"
                     />
 
-                    <div className="absolute inset-0 flex justify-end m-3 card-img_hover gap-2">
-                        <div
-                            onClick={() => window.open(demo_link, '_blank')}
-                            className="green-pink-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-                        >
-                            <img
-                                src={demo}
-                                alt="demo"
-                                className="w-1/2 h-1/2 object-contain"
-                            />
-                        </div>
-
+                    <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
                         <div
                             onClick={() => window.open(source_code_link, '_blank')}
                             className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
                         >
                             <img
                                 src={github}
-                                alt="github"
+                                alt="source code"
                                 className="w-1/2 h-1/2 object-contain"
                             />
                         </div>
@@ -53,12 +54,15 @@ const ProjectCard = props => {
 
                 <div className="mt-4 flex flex-wrap gap-2">
                     {tags.map(tag => (
-                        <div key={tag.name} className="bg-primary rounded-2xl px-3">
-                            <p className={`text-[14px] ${tag.color}`}>#{tag.name}</p>
-                        </div>
+                        <p
+                            key={`${name}-${tag.name}`}
+                            className={`text-[14px] ${tag.color}`}
+                        >
+                            #{tag.name}
+                        </p>
                     ))}
                 </div>
-            </div>
+            </Tilt>
         </motion.div>
     );
 };
