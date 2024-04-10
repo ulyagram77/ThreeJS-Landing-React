39f0645956a579d7d20f03fd12372c85a5089852
whitegolyb <kirillulianow@gmail.com>
2024-04-10 16:28:55 +0300

    added i18n config

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
+import i18n from './utils/i18n';
 
 import 'styles/main.css';
 
